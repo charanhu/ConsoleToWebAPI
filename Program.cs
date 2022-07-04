d2733624d67f9b3d955bd6c7d8bd1e86dc081f46
charanhu <charanhumail@gmail.com>
2022-07-04 15:22:16 +0530

    Append the URL

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ConsoleToWebAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+
+            Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webHost =>
+                {
+                    webHost.UseStartup<Startup>();
+                });
+    }
+}
+// See https://aka.ms/new-console-template for more information
+
---
+++ Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+
+namespace ConsoleToWebAPI
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello from new WEB API APP");
+                });
+
+                //Append the URL
+                endpoints.MapGet("/test", async context =>
+                {
+                    await context.Response.WriteAsync("Hello from new WEB API APP TEST");
+                });
+            });
+        }
+    }
+}
