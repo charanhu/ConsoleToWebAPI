b339bfb7bd391762dec3c40697156f896ee52d6c
charanhu <charanhumail@gmail.com>
2022-07-04 15:54:00 +0530

    Inject WEB API

--- Startup.cs
+++ Startup.cs
@@ -9,23 +9,14 @@ namespace ConsoleToWebAPI
     {
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddControllers();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello from new WEB API APP");
-                });
-
-                //Append the URL
-                endpoints.MapGet("/test", async context =>
-                {
-                    await context.Response.WriteAsync("Hello from new WEB API APP TEST");
-                });
+                endpoints.MapControllers();  //Inject WebAPI
             });
         }
     }
