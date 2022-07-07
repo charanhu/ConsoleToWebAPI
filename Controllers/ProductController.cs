2f2e5316ed953d7857521ad24cf2479f9a3f0c1b
charanhu <charanhumail@gmail.com>
2022-07-07 16:57:35 +0530

    Dependency injection

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleToWebAPI.Models;
+using ConsoleToWebAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleToWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductRepository _productRepository;
+
+        public ProductController()
+        {
+            _productRepository = new ProductRepository();
+        }
+
+        [HttpPost("")]
+        public IActionResult AddProduct([FromBody]ProductModel product)
+        {
+            _productRepository.AddProduct(product);
+            var products = _productRepository.GetAllProducts();
+            return Ok(products);
+        }
+    }
+}
