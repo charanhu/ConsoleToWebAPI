e0c2cb4d58878d684672c9d56ecad498f534c288
charanhu <charanhumail@gmail.com>
2022-07-07 11:37:05 +0530

    FromHeader

---
+++ Controllers/CountriesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleToWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        [HttpPost("{id}")]
+        public IActionResult AddCountry([FromRoute]int id, [FromHeader]string developer, [FromHeader]string course)
+        {
+            return Ok($"Name = {course}");
+        }
+    }
+}
